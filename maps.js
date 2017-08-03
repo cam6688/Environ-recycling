7a7342cee03b49fe7ee275cf5f141b76247c9b1a
Latisha Sumardy <lsmarty23@gmail.com>
2017-08-03 14:30:30 -0700

    i also don't know what i changed

--- maps.js
+++ maps.js
@@ -72,7 +72,7 @@
           url: place.icon,
           size: new google.maps.Size(71, 71),
           origin: new google.maps.Point(0, 0),
-          anchor: new google.maps.Point(17, 34),
+          anchor: new google.maps.Point(0, 34),
           scaledSize: new google.maps.Size(25, 25)
         };
 
