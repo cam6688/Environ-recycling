7625056d7b3e64b6ff88d7967d38640c7e1dc35c
Latisha Sumardy <lsmarty23@gmail.com>
2017-08-04 10:35:52 -0700

    testing new map

---
+++ newmap.js
@@ -0,0 +1,26 @@
+var markersData = [
+  {
+      lat: 37.7760,
+      lng: 121.9702,
+      name: "Access Metals Trading Inc.",
+      address1:"Rua Diogo Cão, 125",
+      address2: "Praia da Barra",
+      postalCode: "3830-772 Gafanha da Nazaré" // don’t insert comma in the last item of each marker
+   },
+   {
+      lat: 40.59955,
+      lng: -8.7498167,
+      name: "Camping Costa Nova",
+      address1:"Quinta dos Patos, n.º 2",
+      address2: "Praia da Costa Nova",
+      postalCode: "3830-453 Gafanha da Encarnação" // don’t insert comma in the last item of each marker
+   },
+   {
+      lat: 40.6247167,
+      lng: -8.7129167,
+      name: "Camping Gafanha da Nazaré",
+      address1:"Rua dos Balneários do Complexo Desportivo",
+      address2: "Gafanha da Nazaré",
+      postalCode: "3830-225 Gafanha da Nazaré" // don’t insert comma in the last item of each marker
+   } // don’t insert comma in last item
+];
