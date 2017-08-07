526564fe54780b3c931c6d0003d56bb9e2c92d14
Latisha Sumardy <lsmarty23@gmail.com>
2017-08-07 10:06:51 -0700

    trying new function

--- navscript.js
+++ navscript.js
@@ -12,3 +12,14 @@ $(document).ready(function () {
 });
 
 //onload="setTimeout(openNav(), 300);"
+function run(interval, frames) {
+    var int = 1;
+
+    function func() {
+        document.body.id = "b"+int;
+        int++;
+        if(int === frames) { int = 1; }
+    }
+
+    var swap = window.setInterval(func, interval);
+}
